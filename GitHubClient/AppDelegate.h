3e0defe6c73b0fcaf4e5419f681287da68bad7a4
Erik Sundin <erik@eriksundin.se>
2014-11-25 20:34:42 +0100

    Initial Commit

---
+++ GitHubClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubClient
+//
+//  Created by Erik Sundin on 25/11/14.
+//  Copyright (c) 2014 Erik Sundin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitHubClient
+//
+//  Created by Erik Sundin on 25/11/14.
+//  Copyright (c) 2014 Erik Sundin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
